f06b37ef8f4c4bf8001e1d22c1463c730814b083
bubblecocoa <bubblecocoa@gmail.com>
2023-05-12 23:10:51 +0900

    feat: MVC 스터디 사전작업

---
+++ 03_MVC/BMO/Experiment_MVC/Experiment_MVC/ViewController.swift
@@ -0,0 +1,37 @@
+//
+// © 2021. yagom academy all rights reserved
+// This tutorial is produced by Yagom Academy and is prohibited from redistributing or reproducing.
+//
+
+import UIKit
+
+struct Registrant {
+
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    var registrantList: [Registrant] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func hitRegisterButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func hitCheckButton(_ sender: Any) {
+        
+    }
+    
+    func register() {
+        
+    }
+}
