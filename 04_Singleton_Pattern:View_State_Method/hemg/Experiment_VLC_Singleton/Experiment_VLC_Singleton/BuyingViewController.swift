6b46930a7fdb275c2fcd64844a4fdc1757ff973e
hemg2 <hemg2@naver.com>
2023-05-20 00:26:25 +0900

    refactor: init 실험 진행, 화면전환 push

--- 04_Singleton_Pattern:View_State_Method/hemg/Experiment_VLC_Singleton/Experiment_VLC_Singleton/BuyingViewController.swift
+++ 04_Singleton_Pattern:View_State_Method/hemg/Experiment_VLC_Singleton/Experiment_VLC_Singleton/BuyingViewController.swift
@@ -11,25 +11,25 @@ class BuyingViewController: UIViewController {
         super.viewDidLoad()
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        print("22222=====  main  viewWillAppear")
-//    }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("22222===== main viewDidAppear")
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        print("22222===== main viewWillDisappear")
-//    }
-//
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidDisappear(animated)
-//        print("22222===== main viewDidDisappear")
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("22222=====  main  viewWillAppear")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("22222===== main viewDidAppear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("22222===== main viewWillDisappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("22222===== main viewDidDisappear")
+    }
     
     @IBAction func secondDismissButton(_ sender: UIButton) {
         PointManager.shared.point -= 1
--- 04_Singleton_Pattern:View_State_Method/hemg/Experiment_VLC_Singleton/Experiment_VLC_Singleton/MainViewController.swift
+++ 04_Singleton_Pattern:View_State_Method/hemg/Experiment_VLC_Singleton/Experiment_VLC_Singleton/MainViewController.swift
@@ -19,6 +19,24 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var numberLabel: UILabel!
     
+    
+//    init() {
+//        super.init()
+//    }  안됨요
+    
+        // required init 공부 진행 시켜~
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+//        fatalError("init(coder:) has not been implemented")
+        print("내가 바로 짜파게티 요리사")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("응 오늘 토요일이야 너 짤렸어")
+        print("내가 2등이야")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("11111===== main viewDidLoad")
@@ -43,19 +61,19 @@ class MainViewController: UIViewController {
 
     }
 
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("11111===== main viewDidAppear")
-//    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("11111===== main viewDidAppear")
+    }
 
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        print("11111===== main viewWillDisappear")
-//    }
-//
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidDisappear(animated)
-//        print("11111===== main viewDidDisappear")
-//    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("11111===== main viewWillDisappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("11111===== main viewDidDisappear")
+    }
 }
 
