414e5b27e23fe248735af6f7fce161b9dbb59e41
bubblecocoa <bubblecocoa@gmail.com>
2023-05-20 00:02:51 +0900

    feat: 4주차 스터디 Step1, Step2

--- 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/BuyingViewController.swift
+++ 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/BuyingViewController.swift
@@ -9,9 +9,31 @@ class BuyingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("BuyingViewController viewDidLoad")
     }
     
-    @IBAction func dismissButton(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        print("BuyingViewController viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("BuyingViewController viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("BuyingViewController viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("BuyingViewController viewDidDisappear")
+    }
+    
+    @IBAction func dismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapBuyButton(_ sender: UIButton) {
+        PointManager.shared.point -= 1
         dismiss(animated: true, completion: nil)
     }
 }
--- 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/MainViewController.swift
+++ 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/MainViewController.swift
@@ -6,14 +6,44 @@
 import UIKit
 
 class PointManager {
+    static let shared = PointManager()
     var point: Int = 50
+    
+    private init() {}
 }
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var pointLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setPointLabel()
+        print("MainViewController viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("MainViewController viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("MainViewController viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("MainViewController viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("MainViewController viewDidDisappear")
+    }
+    
+    @IBAction func tapRefreshButton(_ sender: UIButton) {
+        setPointLabel()
+    }
+    
+    func setPointLabel() {
+        pointLabel.text = String(PointManager.shared.point)
     }
 }
 
