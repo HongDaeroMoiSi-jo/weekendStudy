883c610118b7c8d08df3c478112de5296903d80d
bubblecocoa <bubblecocoa@gmail.com>
2023-05-19 09:23:15 +0900

    feat: Singleton Pattern / View State Method 스터디 사전작업

---
+++ 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/BuyingViewController.swift
@@ -0,0 +1,17 @@
+//
+// © 2021. yagom academy all rights reserved
+// This tutorial is produced by Yagom Academy and is prohibited from redistributing or reproducing.
+//
+
+import UIKit
+
+class BuyingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismissButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ 04_Singleton_Pattern:View_State_Method/BMO/Experiment_VLC_Singleton/Experiment_VLC_Singleton/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+// © 2021. yagom academy all rights reserved
+// This tutorial is produced by Yagom Academy and is prohibited from redistributing or reproducing.
+//
+
+import UIKit
+
+class PointManager {
+    var point: Int = 50
+}
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
