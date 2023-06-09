6c40d055ad0dfff0266bea0c1cc1fb2678ef07b2
Hoon94 <dleognsdl1@naver.com>
2023-06-10 00:50:01 +0900

    feat: STEP 1

--- 07_Higher_Order_Function/hoon/HigherOrderFunction.playground/Contents.swift
+++ 07_Higher_Order_Function/hoon/HigherOrderFunction.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct GameCharacter {
+    let skill1: () -> Void = {
+        print("파이어")
+    }
+    let skill2: () -> Void = {
+        print("아이스")
+    }
+    let skill3: () -> Void = {
+        print("워터")
+    }
+    let skill4: () -> Void = {
+        print("윈드")
+    }
+    
+    func tabShiftKey(skill: () -> Void) {
+        skill()
+    }
+}
+
+let wizard: GameCharacter = GameCharacter()
+wizard.tabShiftKey(skill: wizard.skill1)
+wizard.tabShiftKey(skill: wizard.skill2)
+wizard.tabShiftKey(skill: wizard.skill3)
+wizard.tabShiftKey(skill: wizard.skill4)
