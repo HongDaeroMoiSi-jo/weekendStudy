d8c10c403f35c044f19c433907e80fa3e430502b
Hoon94 <dleognsdl1@naver.com>
2023-06-10 11:43:07 +0900

    feat: STEP 3

--- 07_Higher_Order_Function/hoon/HigherOrderFunction.playground/Contents.swift
+++ 07_Higher_Order_Function/hoon/HigherOrderFunction.playground/Contents.swift
@@ -14,13 +14,64 @@ struct GameCharacter {
         print("윈드")
     }
     
-    func tabShiftKey(skill: () -> Void) {
-        skill()
+    func a(number: Int) -> Void {
+        print(number)
+    }
+    
+    func tabShiftKey(number: Int, skill: (Int) -> Void) {
+        skill(number)
     }
 }
 
 let wizard: GameCharacter = GameCharacter()
-wizard.tabShiftKey(skill: wizard.skill1)
-wizard.tabShiftKey(skill: wizard.skill2)
-wizard.tabShiftKey(skill: wizard.skill3)
-wizard.tabShiftKey(skill: wizard.skill4)
+//wizard.tabShiftKey(skill: wizard.skill1)
+//wizard.tabShiftKey(skill: wizard.skill2)
+//wizard.tabShiftKey(skill: wizard.skill3)
+//wizard.tabShiftKey(skill: wizard.skill4)
+
+wizard.tabShiftKey(number: 10, skill: wizard.a)
+wizard.tabShiftKey(number: 10) {num in
+    wizard.a(number: num)
+}
+
+var arrayForHemg = [1, 2, 3, 4]
+
+arrayForHemg.map(Double.init)
+arrayForHemg.map{ Double.init($0) }
+
+
+extension String {
+    func myFilter(_ isIncluded: (Character) -> Bool) -> [String] {
+        var result: [String] = []
+        
+        for i in self {
+            let isTrue = isIncluded(i)
+            if isTrue {
+                result.append(String(i))
+            }
+        }
+        
+        return result
+    }
+    
+    func myReduce(_ initResult: String, _ method: (String, String) -> String) -> String {
+        var result: String = initResult
+        
+        for i in self {
+            result = method(result, String(i))
+        }
+        
+        return result
+    }
+}
+
+"2re3sd4sd5d6".myFilter {
+    $0.isNumber
+}
+
+"123".reduce("0", { $0 + "번" + String($1) })
+
+"123".myReduce("0", { $0 + "번" + $1 })
+//0번1
+//0번1번2
+//0번1번2번3
