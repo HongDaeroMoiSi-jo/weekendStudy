c557a9cad5028db6fe59d051912a8283f3a5549b
Hoon94 <dleognsdl1@naver.com>
2023-07-15 04:23:58 +0900

    feat: step2 MacBook

--- 10_POP/hoon/POP/POP/main.swift
+++ 10_POP/hoon/POP/POP/main.swift
@@ -7,3 +7,17 @@
 
 typealias WattPerHour = Int
 typealias Watt = Int
+
+struct MacBook {
+    let wattPerHour: WattPerHour = 65
+    var currentBattery: WattPerHour
+    let maxBattery: WattPerHour = 100
+    
+    func chargeBattery(charger: Chargeable) {
+        let currentWattPerHour = charger.convert(chargeableWattPerHour: wattPerHour)
+        let needBattery = maxBattery - currentBattery
+        let time = needBattery / currentWattPerHour
+        
+        print("걸린 시간: \(time)")
+    }
+}
