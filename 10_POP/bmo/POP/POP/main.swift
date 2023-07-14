6790fcb35efc890e6bc68a0c7e33a45fcbced994
bubblecocoa <bubblecocoa@gmail.com>
2023-07-15 08:48:31 +0900

    feat: POP 스터디 step2 맥북 & 충전기 인스턴스 생성 및 결과 조회

--- 10_POP/bmo/POP/POP/main.swift
+++ 10_POP/bmo/POP/POP/main.swift
@@ -4,3 +4,17 @@
 //
 //  Created by 비모 on 2023/07/14.
 //
+
+let appleWatchCharger = Charger(maximumWattPerHour: 5)
+let iPhoneFastCharger = Charger(maximumWattPerHour: 18)
+let iPadCharger = Charger(maximumWattPerHour: 30)
+let macBookCharger = Charger(maximumWattPerHour: 96)
+let macBookFastCharger = Charger(maximumWattPerHour: 106)
+
+var macBook = MacBook(allowChargeWatt: 65, currentBatteryCapacity: 30)
+
+macBook.chargeBattery(charger: appleWatchCharger)
+//macBook.chargeBattery(charger: iPhoneFastCharger)
+//macBook.chargeBattery(charger: iPadCharger)
+//macBook.chargeBattery(charger: macBookCharger)
+//macBook.chargeBattery(charger: macBookFastCharger)
