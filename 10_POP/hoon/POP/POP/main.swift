25f7bef8e8e9bccce517a82117e0a0cec0746ca3
Hoon94 <dleognsdl1@naver.com>
2023-07-15 04:32:05 +0900

    feat: step2 Charger

--- 10_POP/hoon/POP/POP/main.swift
+++ 10_POP/hoon/POP/POP/main.swift
@@ -7,3 +7,17 @@
 
 typealias WattPerHour = Int
 typealias Watt = Int
+
+struct Charger: Chargeable {
+    var maximumWattPerHour: WattPerHour
+    
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour {
+        return chargeableWattPerHour < maximumWattPerHour ? chargeableWattPerHour : maximumWattPerHour
+    }
+}
+
+let 애플워치충전기 = Charger(maximumWattPerHour: 5)
+let 아이폰고속충전기 = Charger(maximumWattPerHour: 18)
+let 아이패드충전기 = Charger(maximumWattPerHour: 30)
+let 맥북충전기1 = Charger(maximumWattPerHour: 96)
+let 맥북충전기2 = Charger(maximumWattPerHour: 106)
