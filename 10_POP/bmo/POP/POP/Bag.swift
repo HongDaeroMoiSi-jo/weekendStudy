df4d9a69f5b0c54fbef4b4d6131d1d13becf804f
bubblecocoa <bubblecocoa@gmail.com>
2023-07-15 08:51:44 +0900

    feat: POP 스터디 step3 Bag 구조체 생성

---
+++ 10_POP/bmo/POP/POP/Bag.swift
@@ -0,0 +1,14 @@
+//
+//  Bag.swift
+//  POP
+//
+//  Created by 비모 on 2023/07/15.
+//
+
+struct Bag {
+    private var items: [Portable] = []
+    
+    mutating func put(item: Portable) {
+        items.append(item)
+    }
+}
