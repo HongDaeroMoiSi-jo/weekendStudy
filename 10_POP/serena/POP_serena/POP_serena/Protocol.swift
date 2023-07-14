4c1fa9bdf96eb461b507e8a1f0fbf8abcb34dc6c
Serena <sha0720@naver.com>
2023-07-15 08:15:46 +0900

    feat: step1

---
+++ 10_POP/serena/POP_serena/POP_serena/Protocol.swift
@@ -0,0 +1,15 @@
+//
+//  Protocol.swift
+//  POP_serena
+//
+//  Created by Serena on 2023/07/15.
+//
+
+typealias WattPerHour = Int
+typealias Watt = Int
+
+protocol Chargeable {
+    var maximumWattPerHour: WattPerHour { get set }
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
+    
+}
---
+++ 10_POP/serena/POP_serena/POP_serena/main.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  POP_serena
+//
+//  Created by Serena on 2023/07/15.
+//
+
