ee90a4ae15ec097c44e84ceaf3db2d463e700773
bubblecocoa <bubblecocoa@gmail.com>
2023-10-07 10:03:46 +0900

    feat: MVC&MVVM 스터디 폴더 초기화

---
+++ 20_MVC&MVVM/BMO/MVC&MVVM/MVC&MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC&MVVM
+//
+//  Created by BMO on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
